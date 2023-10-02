60e5e816d94a53ebefb0ef42f2f7f42df354d371
Mathis Boulais <https://github.com/Asukyy>
2023-10-02 15:42:54 +0200

    2eme partie

---
+++ 2eme partie/exo3/exo3.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+public class exo3 {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> quotation = new ArrayList<>();
+        quotation.add("\"Quand Paris s’enrhume, l’Europe a froid: ");
+        quotation.add("S'il y a un diamant dans la poitrine, il brille sur le visage: ");
+        quotation.add("\" La joie de vivre n'est pas un but, mais un\r\n" + //
+                "devoir: ");
+
+                for(int i = 0; i<quotation.size(); i++){
+                    System.out.println(quotation.get(i) + countVowels(quotation.get(i)));
+                }
+
+
+    }
+
+
+    private static int countVowels(String str) {
+        Integer Vowels = 0;
+        for(int i = 0; i<str.length(); i++){
+            if(str.charAt(i) == 'a' || str.charAt(i) == 'e' ||
+             str.charAt(i) == 'i' || str.charAt(i) == 'o' || str.charAt(i) == 'u'){
+                Vowels++;
+        }
+
+    }
+
+    return Vowels;
+
+
+
+}
+}
