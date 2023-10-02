1f3d83d7a015255d5677d9d45838bc227a4f6a43
Mathis Boulais <https://github.com/Asukyy>
2023-10-02 11:19:25 +0200

    exo456

---
+++ exo4/exo4.java
@@ -0,0 +1,18 @@
+package exo4;
+
+public class exo4 {
+    public static void main(String[] args) {
+
+        String str = "Hello World !";
+        int i = 0;
+
+        while (i < str.length()) {
+            System.out.print(str.charAt(i));
+            if (i < str.length()) {
+                System.out.print(" ");
+            }
+            i++;
+        }
+    }
+
+}
