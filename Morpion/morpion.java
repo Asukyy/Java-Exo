bcca5320e6fc1751c1ee617064d1019502f2a3c5
Mathis Boulais <https://github.com/Asukyy>
2023-10-05 10:41:30 +0200

    .

--- Morpion/morpion.java
+++ Morpion/morpion.java
@@ -13,6 +13,8 @@ public class MorpionGUI extends JFrame implements ActionListener {
         grille = new int[3][3];
         currentPlayer = 1;
 
+        JOptionPane.showMessageDialog(this, "Bienvenue dans le jeu du morpion !\nVous allez jouer contre l'ordinateur.\nBonne chance !");
+
         buttons = new JButton[3][3];
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
